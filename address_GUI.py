9a89a5a8a434d162686f3c8b849624b8580259aa
Julie <julie.hoang.viet.nguyen@ocsbstudent.ca>
2017-09-12 19:33:12 -0400

    Initial Address Commit

---
+++ address_GUI.py
@@ -0,0 +1,10 @@
+# Created by: Julie Nguyen
+# Created on: Sept 2017
+# Created for: ICS3U
+# Daily Assignment - Unit 0-02
+# This program is the personal address program, but as a GUI
+
+import ui
+
+v = ui.load_view()
+v.present('sheet')
